f13195267d0ce35360d52c391e290524f05e3741
James Whittaker <jmwhittaker@gmail.com>
2017-10-05 22:39:47 +0100

    Added AlbumDetail component and props

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     return fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => response.json())
       .then((responseJson) => {
-        console.log(responseJson);
+        this.setState({ albums: responseJson });
       })
       .catch((error) => {
         console.error(error);
       });
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
-    return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-    );
+    console.log(this.state);
+
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
