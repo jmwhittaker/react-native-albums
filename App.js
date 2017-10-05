74c5747d193d771deb3a1f3c8db2a80befc61241
James Whittaker <jmwhittaker@gmail.com>
2017-10-05 11:54:19 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+// Import a Library to help create component
+
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create Component
+
+const HelloWorld = () => <Text>Hello World</Text>;
+
+export default HelloWorld;
