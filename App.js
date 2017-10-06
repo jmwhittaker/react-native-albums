1a1694f615b2ae0317b4c1e64c6e0e816485ec19
James Whittaker <jmwhittaker@gmail.com>
2017-10-06 11:24:36 +0100

    Added scrollview

--- App.js
+++ App.js
@@ -6,9 +6,10 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 // Create Component
+// Flex:1 is a gotcha for scrollview to correctly scroll
 
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
